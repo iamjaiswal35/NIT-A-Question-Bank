96c2f0f751d434d9d0b01340fa43ee83f473e728
iamjaiswal35 <karankumar35980@gmail.com>
2023-08-08 09:57:39 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import img from "./nitalogo.png";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="container">
+        <div>
+          <img src={img} alt="logo" class="logo"/>
+        </div>
+          <div class="inbox">
+            <div class="intup-style">
+              <input class="user" placeholder='UserName' type="text"></input>
+            </div>
+            <div class="intup-style">
+              <input class="pass" placeholder='PassWord' type="text"></input>
+            </div> 
+            <div class="use">
+              
+            </div>
+            <div class="pass">
+
+            </div>
+          </div>
+      </div>
     </div>
   );
 }
